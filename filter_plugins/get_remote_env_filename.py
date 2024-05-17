7ceea15857be507c3ed31499a8bd07d843590da9
marc7s <marcus.schagerberg@gmail.com>
2024-05-17 03:10:36 +0200

    Made stripping of env name in environment file name optional

--- filter_plugins/get_remote_env_filename.py
+++ filter_plugins/get_remote_env_filename.py
@@ -4,14 +4,14 @@ class FilterModule(object):
     def filters(self):
         return {'get_remote_env_filename': self.get_remote_env_filename}
 
-    def get_remote_env_filename(self, env_filename, env_name):
+    def get_remote_env_filename(self, env_filename, env_name, strip_env_file_name = True):
         remote_env_filename = env_filename.replace(".vault", "")
         
         # Handle Angular environment files
         if 'environment' in remote_env_filename:
             remote_env_filename = f'src/environments/{remote_env_filename}'
         # Handle normal .env files
-        else:
+        elif strip_env_file_name:
             remote_env_filename = remote_env_filename.replace(f'.{env_name}', '')
         
         return remote_env_filename
